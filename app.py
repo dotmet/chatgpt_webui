a0f407a2dff49d6f9d5d3b7a9d90379c88c1faf9
Mension <72541408+dotmet@users.noreply.github.com>
2023-02-17 15:01:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ import gradio as gr
 from revChatGPT.V1 import Chatbot
 
 #You can setup login information here, or login in from UI
-email = None
-password = None
-access_token = None
-session_token = None
 
-# Detect if this script is running on the google colab
-def is_google_colab():
-    try:
-        import google.colab
-        return True
-    except:
-        return False
+# If you want to use Email/Password to login, put your account information here
+email = ""
+password = ""
+
+# If you have an access token, put your access token here
+access_token = ""
+
+# If you have a session token, put your session token here
+session_token = ""
 
 def configure_chatbot(method, info):
     
@@ -60,9 +58,11 @@ def chatgpt_clone(inputs, history):
 with gr.Blocks() as demo:
     gr.Markdown("""<h1><center>ChatGPT BOT build by revChatGPT & Gradio</center></h1>
     """)
+    gr.Markdown(
+        "#### Author: [dotmet](https://github.com/dotmet)  Github link:[ChatGPTWEB](https://github.com/dotmet/chatgpt_webui)")
 
     if not ((email and password) or access_token or session_token):
-        gr.Markdown("""<h2>Login to OpenAI<h2>""")
+        gr.Markdown("""<h2>Login to OpenAI</h2>""")
         with gr.Row():
             with gr.Group():
                 method = gr.Dropdown(label="Login Method", choices=login_method)
@@ -70,12 +70,23 @@ with gr.Blocks() as demo:
                 with gr.Row():
                     login = gr.Button("Login")
                     login.click(configure_chatbot, inputs=[method, info])
+    else:
+        if email and password:
+            method = "Email/Password"
+            info = email + " " + password
+        elif access_token:
+            method = "Access token"
+            info = access_token
+        elif session_token:
+            method = "Session token"
+            info = session_token
+        configure_chatbot(method, info)
 
-    gr.Markdown("""<h2>Start Chatting ...<h2>""")
+    gr.Markdown("""<h2>Start Chatting ...</h2>""")
     chatbot1 = gr.Chatbot()
     message = gr.Textbox(placeholder="Chat here")
     state = gr.State()
     submit = gr.Button("SEND")
     submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot1, state])
 
-    demo.launch(debug = True, share=is_google_colab())
+    demo.launch(debug = True, share=False)
